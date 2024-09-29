a6742c9a9515fcbe98b6abf63a159612e250173a
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-09-29 22:00:00 +0530

    create the pending and splash screens

---
+++ app/src/main/java/com/example/ssd_e_commerce/AccountPendingActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ssd_e_commerce
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AccountPendingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_pending)
+
+        // Add logic to handle the "Contact support" click event
+    }
+}
---
+++ app/src/main/java/com/example/ssd_e_commerce/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ssd_e_commerce
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Add logic to navigate to the next screen after a delay
+    }
+}
